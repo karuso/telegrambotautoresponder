e2fd8461abf6598e7e05afe98fed3d935c3503e6
Alessandro Caruso <a.caruso@superbatlc.it>
2020-03-16 14:44:57 +0100

    Add keyboard response when mopre than one tag is found

--- bot.js
+++ bot.js
@@ -6,12 +6,17 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
 var fs = require('fs');
 var util = require('util');
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'a'});
+var feedback_file = fs.createWriteStream(__dirname + '/feedback.log', {flags : 'a'});
 var log_stdout = process.stdout;
 
 console.log = function(d) { //
   log_file.write(util.format(d) + '\n');
   log_stdout.write(util.format(d) + '\n');
 };
+
+console.feeback = function(d) {
+    feedback_file.write(util.format(d) + '\n');
+}
 // ---------- LOGGIN FEATURES END ----------
 
 // ---------- FAST ANSWERS ----------
@@ -19,74 +24,127 @@ var fastAnswers = JSON.parse(fs.readFileSync('./answers.json', 'utf8'));
 // ---------- FAST ANSWERS END ----------
 
 console.log("[DEBUG] Bot is starting...");
+console.feeback("[FEEDBACK] Question and asnwer file");
+console.feeback("Format");
+console.feeback("Q:<question><tab>A:<answer>");
+console.feeback("-----------------------------------------------------------");
 
+
+keyboard = function(array_of_tags) {
+    let markup = {
+        "reply_markup": {
+            "keyboard": [array_of_tags],
+            "resize_keyboard": true, 
+            "one_time_keyboard": true
+        }
+    }
+    return markup;
+}
 
 bot.on('message', (msg) => {  
      //bot.sendMessage(msg.chat.id, "Hello dear user"); 
-	 //console.log(msg);
-	 let date = new Date(msg.date * 1000);
-	 let timestamp = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + "@" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	 
-	
-	let msgFromInfo = "";
-	if(msg.chat.type == "private"){
-		msgFromInfo = msg.from.first_name + "(" + msg.from.id + ")";
-	}else if(msg.chat.type == "group"){
-		msgFromInfo = msg.from.first_name + "(" + msg.from.id + "/" + msg.chat.title + ")";
-	}
-	
-	 console.log("[INFO](" + timestamp + ") Msg from " + msgFromInfo + ": " + msg.text);
-	 
-	 if(msg.text != null){
-		let mex = controlMessage(msg.text);
-		if(mex != null){
-			//bot.sendMessage(msg.chat.id, mex);
-					
-			if(mex.type == "text"){
-				bot.sendMessage(msg.chat.id, mex.reply);
-			}else if(mex.type == "image"){
-				if(mex.reply.includes("gif")){
-					bot.sendVideo(msg.chat.id, mex.reply);
-				}else{
-					bot.sendPhoto(msg.chat.id, mex.reply);
-				}
-			}else if(mex.type == "audio"){
-				//bot.sendAudio(msg.chat.id, mex.reply, "Xfox Assistant Bot", "Title", "Caption");
-				bot.sendVoice(msg.chat.id, mex.reply);
-			}else if(mex.type == "function"){
-				mex.reply(bot); //let function handle the answer
-			}
-		 }
-	 }
+     //console.log(msg);
+     let date = new Date(msg.date * 1000);
+     let timestamp = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + "@" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+     
+    
+    let msgFromInfo = "";
+    if(msg.chat.type == "private"){
+        msgFromInfo = msg.from.first_name + "(" + msg.from.id + ")";
+    }else if(msg.chat.type == "group"){
+        msgFromInfo = msg.from.first_name + "(" + msg.from.id + "/" + msg.chat.title + ")";
+    }
+    
+     console.log("[INFO](" + timestamp + ") Msg from " + msgFromInfo + ": " + msg.text);
+     
+     if(msg.text != null){
+        // let mex = controlMessage(msg.text);
+        let results = advancedControlMessage(msg.text); // bool, array
+
+        if(results[0]) {
+            let mex = results[1];
+            if(mex.length > 1) {
+                bot.sendMessage(msg.chat.id, "Scusa non capisco quali di questi argomenti ti interessino", keyboard(mex));
+            } else {
+                mex = mex[0];
+                // console.log(util.inspect(mex, false, null, true /* enable colors */));
+                if(mex.type == "text"){
+                    bot.sendMessage(msg.chat.id, mex.reply);
+                    console.feeback("Q:" + msg.text + "\tA:" + mex.reply);
+                }else if(mex.type == "image"){
+                    if(mex.reply.includes("gif")){
+                        bot.sendVideo(msg.chat.id, mex.reply);
+                    }else{
+                        bot.sendPhoto(msg.chat.id, mex.reply);
+                    }
+                }else if(mex.type == "audio"){
+                    //bot.sendAudio(msg.chat.id, mex.reply, "Xfox Assistant Bot", "Title", "Caption");
+                    bot.sendVoice(msg.chat.id, mex.reply);
+                }
+            }
+        }else{
+            console.feeback("Q:" + msg.text + "\tA: NO ANSWER");
+            bot.sendMessage(msg.chat.id, "Per info visita http://salute.gov.it");
+        }
+     }
 });
 
+
 function controlMessage(message){
-	
-	/*
-	for (const [key, phrases] of Object.entries(fastAnswers)) { //Check for fast answers
-		if(message.toLowerCase().includes(key)){
-			let rnd = Math.floor((Math.random() * (phrases.length-1)) + 0);
-			return phrases[rnd];
-		}
-	}
-	*/
-	
-	let found = null;
-	//TODO: Should substitute forEach with for (const [triggers, oneFastAnswer] of Object.entries(fastAnswers)), in order to use return inside the loop
-	fastAnswers.forEach(function(fastAnswer){ //For every fast answer
-		fastAnswer.triggers.forEach(function(trigger){ //Check among all triggers
-			let regex = new RegExp("\\b"+trigger+"\\b", "gi");//Search global and case insenstive
-			let regexResult = message.match(regex);
-			
-			//console.log("Regex result: " + regexResult);
-			
-			if((regexResult != null) && !found){ //If RegEx matches and wasn't previously found
-				let rnd = Math.floor((Math.random() * (fastAnswer.replies.length)) + 0);
-				found = fastAnswer.replies[rnd];
-				//return fastAnswer.replies[rnd]; //Can't do this with forEach (ahw man, that sucks), see comment above, substitute forEach with for
-			}	
-		});
-	});
-	
-	return found;
+    
+    /*
+    for (const [key, phrases] of Object.entries(fastAnswers)) { //Check for fast answers
+        if(message.toLowerCase().includes(key)){
+            let rnd = Math.floor((Math.random() * (phrases.length-1)) + 0);
+            return phrases[rnd];
+        }
+    }
+    */
+    
+    let found = null;
+    //TODO: Should substitute forEach with for (const [triggers, oneFastAnswer] of Object.entries(fastAnswers)), in order to use return inside the loop
+    fastAnswers.forEach(function(fastAnswer){ //For every fast answer
+        fastAnswer.triggers.forEach(function(trigger){ //Check among all triggers
+            let regex = new RegExp("\\b"+trigger+"\\b", "gi");//Search global and case insenstive
+            let regexResult = message.match(regex);
+            
+            //console.log("Regex result: " + regexResult);
+            
+            if((regexResult != null) && !found){ //If RegEx matches and wasn't previously found
+                let rnd = Math.floor((Math.random() * (fastAnswer.replies.length)) + 0);
+                found = fastAnswer.replies[rnd];
+                //return fastAnswer.replies[rnd]; //Can't do this with forEach (ahw man, that sucks), see comment above, substitute forEach with for
+            }
+        });
+    });
+    
+    return found;
 }
+
+function advancedControlMessage(message){
+    let found = false;
+    let found_tags = [];
+    let reply = '';
+    //TODO: Should substitute forEach with for (const [triggers, oneFastAnswer] of Object.entries(fastAnswers)), in order to use return inside the loop
+    fastAnswers.forEach(function(fastAnswer){ //For every fast answer
+        fastAnswer.triggers.forEach(function(trigger){ //Check among all triggers
+            let regex = new RegExp("\\b"+trigger+"\\b", "gi");//Search global and case insenstive
+            let regexResult = message.match(regex);
+            
+            if(regexResult != null){
+                found = true;
+                found_tags.push(regexResult[0])
+
+                let rnd = Math.floor((Math.random() * (fastAnswer.replies.length)) + 0);
+                reply = fastAnswer.replies[rnd]
+            }
+        });
+    });
+
+    if(found && found_tags.length === 1) {        
+        return [true, [reply]];
+    }
+    
+    return [found, found_tags];   // values.first, values.second
+}
+
